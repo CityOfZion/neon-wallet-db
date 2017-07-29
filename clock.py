4924d37003d8fd75e1c45a70a3c62ec80d3d0f0c
Ethan Fast <Ejhfast@gmail.com>
2017-07-29 13:31:58 -0700

    cleanup

--- clock.py
+++ clock.py
@@ -1,7 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from rq import Queue
 from api import redis_db as conn
-from api.blockchain import storeLatestBlockInDB, getBlockCount, blockchain_db, storeBlockInDB, checkSeeds
+from api.blockchain import storeLatestBlockInDB, getBlockCount, blockchain_db, storeBlockInDB, checkSeeds, get_highest_node
 
 q = Queue(connection=conn)
 
@@ -20,7 +20,8 @@ def pollNode():
 # intermittantly check for any blocks we missed by polling
 @sched.scheduled_job('interval', seconds=10)
 def syncBlockchain():
-    currBlock = getBlockCount()["result"]
+    nodeAPI = get_highest_node()
+    currBlock = getBlockCount(nodeAPI)["result"]
     lastTrustedBlock = blockchain_db["meta"].find_one({"name":"lastTrustedBlock"})["value"]
     laterBlocks = set([block["index"] for block in blockchain_db["blockchain"].find({"index": {"$gt": lastTrustedBlock}})])
     hash_set = {x:x for x in laterBlocks}
@@ -29,7 +30,7 @@ def syncBlockchain():
     for i in range(lastTrustedBlock+1, currBlock):
         if not i in hash_set:
             print("repairing {}".format(i))
-            q.enqueue(storeBlockInDB, i)
+            q.enqueue(storeBlockInDB, i, nodeAPI)
             stopTrust = True
         if not stopTrust:
             newLastTrusted = i
