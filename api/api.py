83f063d24e8c0499bb3bf2e847b93a8b276f1644
Ethan Fast <Ejhfast@gmail.com>
2017-08-13 02:02:09 -0700

    test

--- api/api.py
+++ api/api.py
@@ -363,40 +363,40 @@ def get_claim(address):
     #         "total_claim": 0,
     #         "total_unspent_claim": 0,
     #         "claims": []})
-    start = time.time()
-    transactions = {t['txid']:t for t in transaction_db.find({"$or":[
-        {"vout":{"$elemMatch":{"address":address}}},
-        {"vin_verbose":{"$elemMatch":{"address":address}}}
-    ]})}
-    print("to get transactions {}".format(time.time() - start))
-    # get sent neo info
-    info_sent = [info_sent_transaction(address, t) for t in transactions.values()]
-    sent_neo = collect_txids(info_sent)["NEO"]
-    # get received neo info
-    info_received = [info_received_transaction(address, t) for t in transactions.values()]
-    received_neo = collect_txids(info_received)["NEO"]
-    unspent_neo = {k:v for k,v in received_neo.items() if not k in sent_neo}
-    # get claim info
-    past_claims = get_past_claims(address)
-    claimed_neo = get_claimed_txids(past_claims)
-    valid_claims = {k:v for k,v in sent_neo.items() if not k in claimed_neo}
-    valid_claims = filter_claimed_for_other_address(valid_claims)
-    block_diffs = compute_claims(valid_claims, transactions)
-    total = sum([x["claim"] for x in block_diffs])
-    # now do for unspent
-    height = get_db_height()
-    start = time.time()
-    unspent_diffs = compute_claims([v for k,v in unspent_neo.items()], transactions, height)
-    print("to compute claims: {}".format(time.time() - start))
-    print(unspent_diffs)
-    unspent_claim_total = sum([x["claim"] for x in block_diffs])
-    print(unspent_claim_total)
+    # start = time.time()
+    # transactions = {t['txid']:t for t in transaction_db.find({"$or":[
+    #     {"vout":{"$elemMatch":{"address":address}}},
+    #     {"vin_verbose":{"$elemMatch":{"address":address}}}
+    # ]})}
+    # print("to get transactions {}".format(time.time() - start))
+    # # get sent neo info
+    # info_sent = [info_sent_transaction(address, t) for t in transactions.values()]
+    # sent_neo = collect_txids(info_sent)["NEO"]
+    # # get received neo info
+    # info_received = [info_received_transaction(address, t) for t in transactions.values()]
+    # received_neo = collect_txids(info_received)["NEO"]
+    # unspent_neo = {k:v for k,v in received_neo.items() if not k in sent_neo}
+    # # get claim info
+    # past_claims = get_past_claims(address)
+    # claimed_neo = get_claimed_txids(past_claims)
+    # valid_claims = {k:v for k,v in sent_neo.items() if not k in claimed_neo}
+    # valid_claims = filter_claimed_for_other_address(valid_claims)
+    # block_diffs = compute_claims(valid_claims, transactions)
+    # total = sum([x["claim"] for x in block_diffs])
+    # # now do for unspent
+    # height = get_db_height()
+    # start = time.time()
+    # unspent_diffs = compute_claims([v for k,v in unspent_neo.items()], transactions, height)
+    # print("to compute claims: {}".format(time.time() - start))
+    # print(unspent_diffs)
+    # unspent_claim_total = sum([x["claim"] for x in block_diffs])
+    # print(unspent_claim_total)
     return jsonify({
         "net": NET,
         "address": address,
-        "total_claim": calculate_bonus(block_diffs),
-        "total_unspent_claim": calculate_bonus(unspent_diffs),
-        "claims": block_diffs})
+        "total_claim": 0, calculate_bonus(block_diffs),
+        "total_unspent_claim": 0, calculate_bonus(unspent_diffs),
+        "claims": []})
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
